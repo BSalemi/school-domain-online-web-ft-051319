56d7b50bf661d2612479e8b629d9c48d36bbafd3
Brian Salemi <briandsalemi@gmail.com>
2019-05-22 18:43:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,28 +2,33 @@ require 'pry'
 
 
 class School
+  class School
 
-  attr_reader :name, :roster, :grade
+    attr_reader :name, :roster, :grade
 
-  def initialize(name)
-    @name = name
-    @roster = {}
-  end
-
-  def add_student(name, grade)
-    if roster[grade] ||= []
-       roster[grade] << name
+    def initialize(name)
+      @name = name
+      @roster = {}
     end
-  end
 
-  def grade(grade)
-    roster[grade]
-  end
-
-  def sort
-    roster.each do |grade, student|
-      student.sort!
+    def add_student(name, grade)
+      if roster[grade] ||= []
+         roster[grade] << name
+      end
+      binding.pry
     end
+
+    def grade(grade)
+      roster[grade]
+    end
+
+    def sort
+      roster.each do |grade, student|
+        student.sort!
+      end
+    end
+
   end
+
 
 end
